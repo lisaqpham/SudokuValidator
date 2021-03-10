d34b16c61dc5a11adf195d6869842a9ef3cd3812
Clarisse Bonang <csbonang@gmail.com>
2021-03-09 23:16:00 -0800

    test_part1&2
    reads from test file and assigns grid to a 2D array

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,8 +1,18 @@
+//
+//  main.cpp
+//  OS_proj2
+//
+//  Created by cpsc on 3/9/21.
+//
+// Reference: https://www.tutorialspoint.com/read-file-line-by-line-using-cplusplus
 
 #include <iostream>
 #include <pthread.h>
+#include <fstream>
+#include <cstring>
 using namespace std;
-int g_sudokuArray[9][9] = {};
+char sudokuArray[30][30] = {};
+int countRow = 0;
 // Check the row
 void *checkRows(void *tRowID)
 {
@@ -29,21 +39,76 @@ void *checkGrids(void *tRowID)
     return NULL;
 }
 
-void ReadFile()
+// updates the array
+void myArray(string line)
+{
+    for(int col = 0; col < line.length(); col++)
+    {
+        sudokuArray[countRow][col] = line[col];
+    }
+    countRow++;
+    cout << "row: " << countRow << endl;
+}
+// prints the array
+void printmyArray()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < 9; j++)
+        {
+            cout << sudokuArray[i][j] << " \n"[j == 8];
+        }
+    }
+}
+void ReadFile(string fileName)
 {
     // read from file
     // Beware:
       // make sure to only pass in integers
       // check when the row stops!! (Recall : array[i][j] where i is the row and j is the column. So, make sure that once you hit the end of the row, increment the i.
     // assign grid to global variable: g_sudokuArray
+    
+    ifstream fileInput;
+    string line = "";
+    int row = 0;
+    int col = 0;
+    fileInput.open(fileName);
+    if(!fileInput.is_open())
+    {
+        cout << "could not open file"<< endl;
+    }
+    else{
+        cout << "HERE" << endl;
+        while(!fileInput.eof())
+        {
+            fileInput >> line;
+            // Diag:
+            //cout << "line " << line << endl;
+            line.erase(remove(line.begin(), line.end(), ','), line.end());
+            myArray(line);
+            
+           
+        }
+    }
+    printmyArray();
+        
+    
 }
 
-
 int main(int argc, const char * argv[]) {
+    string fileName = "";
+    fileName = "Testfile1.txt";
+    // onFS - object of fstream to write to file
+    
     // STEP 1: Create threads
     pthread_t row;
     pthread_t column;
     pthread_t grid;
+    
+    
+    cout << "Welcome to SVP" << endl;
+    cout << "Please enter the file: " << fileName << endl;
+    ReadFile(fileName);
     
     // STEP 2: Read from the test file
                // call ReadFile()
