3e8e1af98df0d17e4f2bfd41e2a8d98fced59310
Lisa Pham <lisaphcm@gmail.com>
2021-03-15 22:41:00 +0700

    Error detection fully working.

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,50 +1,49 @@
-//
-//  main.cpp
-//  mutexLock
-//
-//  Created by cpsc on 3/11/21.
-//
-//#include <unistd.h.>
-// https://dzone.com/articles/how-can-we-controlschedule-execution-of-threads-in
 #include <iostream>
 #include <pthread.h>
 #include <fstream>
 #include <cstring>
+#include <utility>
 #include <stdlib.h>     /* atoi */
 #include <map>
 using namespace std;
+
 int sudokuArray[9][9] = {};
 int zeroArray[9][9]={};
 int countRow = 0;
 int controller = 0;
-void findErrors();
 void printmyArray(int arr[][9], int size);
 bool checkRow(int row, int col);
 bool checkColumn(int row, int col);
 bool checkGrid(int row, int col);
+map<pair<int,int>, int> rcgMap;
 
 void * checkRows(void *arg) {
   int x = 0;
   while(1) {
     if (controller == 0) {
-      // findErrors();
+      cout << "ROW ERRORS" << endl;
       for (int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
+          pair<int, int> coordinate(i,j);
           x = sudokuArray[i][j];
           if (checkRow(i,j) == false) {
+            if (rcgMap[coordinate] >= 1) {
+              rcgMap[coordinate]++;
+            } else {
+              rcgMap[coordinate] = 1;
+            }
+            cout << "Row: " << i + 1 << "    Column: " << j + 1 << "    Wrong Value: " << sudokuArray[i][j] << endl;
             zeroArray[i][j] = 0;
           } else {
-              // temporary: b/f it was = x
             zeroArray[i][j] = 1;
           }
         }
       }
-        // Diag: check if the grid includes corrections
-        printmyArray(zeroArray, 9);
+        //printmyArray(zeroArray, 9);
+        cout << endl;
     }
       break;
   }
-  cout << "check rows\n" << endl;
   controller = 1;
   pthread_exit(NULL);
 }
@@ -52,37 +51,19 @@ void * checkRows(void *arg) {
 void * checkCols(void *arg) {
   while(1) {
     if (controller == 1) {
+      cout << "COLUMN ERRORS" << endl;
       int x = 0;
       for (int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
+          pair<int, int> coordinate(i,j);
           x = sudokuArray[i][j];
           if (checkColumn(i,j) == false) {
-             zeroArray[i][j] = 0;
-          } else {
-            // temporary: b/f it was = x
-            zeroArray[i][j] = 1;
-          }
-        }
-      }
-      // Diag: check if the grid includes corrections
-      printmyArray(zeroArray, 9);
-      break;
-    }
-  }
-  cout << "check columns\n"<< endl;
-  controller = 2;
-  pthread_exit(NULL);
-}
-
-void * checkGrids(void *arg) {
-  while(1) {
-    if (controller == 2) {
-      int x = 0;
-      for (int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-          x = sudokuArray[i][j];
-          if (checkGrid(i,j) == false) {
-            //cout << "Row: " << i << " Col: " << j << endl;
+            if (rcgMap[coordinate] >= 1) {
+              rcgMap[coordinate]++;
+            } else {
+              rcgMap[coordinate] = 1;
+            }
+            cout << "Row: " << i + 1 << "    Column: " << j + 1 << "    Wrong Value: " << sudokuArray[i][j] << endl;
             zeroArray[i][j] = 0;
           } else {
             // temporary: b/f it was = x
@@ -90,12 +71,44 @@ void * checkGrids(void *arg) {
           }
         }
       }
-      // Diag: check if the grid includes corrections
-      printmyArray(zeroArray, 9);
+      //printmyArray(zeroArray, 9);
+      cout << endl;
       break;
     }
   }
-  cout << "check grids\n" << endl;
+  controller = 2;
+  pthread_exit(NULL);
+}
+
+void * checkGrids(void *arg) {
+  while(1) {
+    if (controller == 2) {
+      cout << "GRID ERRORS" << endl;
+      int x = 0;
+      for (int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+          pair<int, int> coordinate(i,j);
+          x = sudokuArray[i][j];
+          if (checkGrid(i,j) == false) {
+            //cout << "Row: " << i << " Col: " << j << endl;
+            if (rcgMap[coordinate] >= 1) {
+              rcgMap[coordinate]++;
+            } else {
+              rcgMap[coordinate] = 1;
+            }
+            cout << "Row: " << i + 1 << "    Column: " << j + 1 << "    Wrong Value: " << sudokuArray[i][j] << endl;
+            zeroArray[i][j] = 0;
+          } else {
+            // temporary: b/f it was = x
+            zeroArray[i][j] = 1;
+          }
+        }
+      }
+      //printmyArray(zeroArray, 9);
+      cout << endl;
+      break;
+    }
+  }
   controller = 0;
   pthread_exit(NULL);
 }
@@ -124,9 +137,6 @@ bool checkColumn(int row, int col) {
   bool noDuplicates = true;
   int curr_row = row;
   int curr_col = col;
-  if (zeroArray[curr_row][curr_col] == 0) {
-    return noDuplicates = false;
-  } else {
     for (int i = 0; i <= 8; i++) {
       if (sudokuArray[i][curr_col] != sudokuArray[curr_row][curr_col]) {
         noDuplicates = true;
@@ -138,59 +148,27 @@ bool checkColumn(int row, int col) {
       }
     }
     return noDuplicates;
-  }
 }
 
 bool checkGrid(int row, int col) {
   bool noDuplicates = true;
   int curr_row = row;
   int curr_col = col;
-  if (zeroArray[curr_row][curr_col] == 0) {
-    return noDuplicates = false;
-  } else {
+
     for (int r = 0; r < 3; r++) {
       for (int c = 0; c < 3; c++) {
         if (((r + curr_row - (curr_row % 3)) == curr_row) && (c + curr_col - (curr_col % 3) == curr_col)) {
           noDuplicates = true;
-        } else if (sudokuArray[r + curr_row - (curr_row % 3)][c + curr_col - (curr_col % 3)] != sudokuArray[curr_row][curr_col]) {
+        } else if ((sudokuArray[r + curr_row - (curr_row % 3)][c + curr_col - (curr_col % 3)]) != (sudokuArray[curr_row][curr_col])) {
           noDuplicates = true;
         } else {
-          noDuplicates = false;
-          break;
+          return noDuplicates = false;
         }
       }
     }
     return noDuplicates;
-  }
+
 }
-
-//make new array with x
-void findErrors() {
-  int x = 0;
-  for (int i = 0; i < 9; i++) {
-    for(int j = 0; j < 9; j++) {
-      x = sudokuArray[i][j];
-      if (checkRow(i,j) == false) {
-        zeroArray[i][j] = 0;
-      } else {
-      //else if (checkColumn(i,j) == false) {
-         // zeroArray[i][j] = 0;
-      //}
-        /*
-      else if (checkGrid(i,j) == false){
-        zeroArray[i][j] = 0;
-      }
-       */
-
-          // temporary: b/f it was = x
-        zeroArray[i][j] = 1;
-      }
-    }
-  }
-  // Diag: check if the grid includes corrections
-  printmyArray(zeroArray, 9);
-}
-
 
 //myArray(): updates the array after reading from file
 void myArray(string line) {
@@ -210,8 +188,8 @@ void printmyArray(int arr[][9], int size) {
   }
 }
 
-//ReadFile(): reads from file
-void ReadFile(string fileName) {
+//readFile(): reads from file
+void readFile(string fileName) {
   ifstream fileInput;
   string line = "";
   fileInput.open(fileName);
@@ -231,15 +209,21 @@ void ReadFile(string fileName) {
   printmyArray(sudokuArray, 9);
 }
 
+void backtracking() {
+  pair<int, int> coordinate(0,0);
+  cout << rcgMap[coordinate] << endl;
+}
+
 int main(int argc, const char * argv[]) {
   pthread_t tid1, tid2, tid3;
   string fileName = "";
-  fileName = "Testfile5.txt";
-  printf("Sudoku Validator Program");
-  cout << "File Name: " << fileName << endl;
-  ReadFile(fileName);
+  fileName = "Testfile4.txt";
+  cout<< "\nSudoku Validator Program" << endl;
+  cout << "File Name: " << fileName << endl << endl;
+  readFile(fileName);
+  cout << endl;
 
-  printf("Before creating the threads\n");
+  //printf("Before creating the threads\n");
   if( pthread_create(&tid1, NULL, checkRows, NULL) != 0 )
         printf("Failed to create thread1\n");
   if( pthread_create(&tid2, NULL, checkCols, NULL) != 0 )
@@ -252,5 +236,6 @@ int main(int argc, const char * argv[]) {
   pthread_join(tid2,NULL);
   pthread_join(tid3,NULL);
   //usleep(10);
+
  exit(0);
 }
